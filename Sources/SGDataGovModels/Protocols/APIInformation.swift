d80c804b10db97a0755281730d2112dd86972288
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-02-01 21:00:01 +0800

    code comment fix

--- Sources/SGDataGovModels/Protocols/APIInformation.swift
+++ Sources/SGDataGovModels/Protocols/APIInformation.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// The `APIInformation` protocol ensures that conforming types include service information and a `URL` resource.
 ///
-/// Types that conform to the `APIInformation` protocol must include a `url` (`URL`) and a `serviceInformation` (`String`).
+/// Types that conform to the `APIInformation` protocol must include a `url` (`URL`) and `serviceInformation` (`String`).
 public protocol APIInformation {
     var url: URL { get }
     var serviceInformation: String { get }
